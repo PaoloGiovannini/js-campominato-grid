5532affe4479a7d2f191db318430b2d1c41e775f
Paolo Giovannini <giovannini13@gmail.com>
2023-03-03 13:54:49 +0100

    esercizio completato

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const containerDom = document.querySelector('.container');
+const bottoneDom = document.querySelector('#bottone')
+const mainDom = document.querySelector('#main')
+const footerDom = document.querySelector('#footer')
+
+bottoneDom.addEventListener('click', function() {
+    mainDom.classList.remove('d-none')
+    footerDom.classList.remove('d-none')
+}
+);
+for (let i = 1; i <= 100; i++ ) {
+    const quadratino = document.createElement('div');
+    quadratino.classList.add('square');
+    containerDom.append(quadratino);
+    quadratino.append(i);
+    quadratino.addEventListener('click', function() {
+        this.classList.toggle('cliccato');
+        console.log(i);
+    }
+    );
+    
+}
+
