5e6633207568bbd2da9a3af740402d4b155046f3
Paolo Giovannini <giovannini13@gmail.com>
2023-03-04 10:56:42 +0100

    aggiunto reset al click di inizio partita

--- js/main.js
+++ js/main.js
@@ -6,18 +6,23 @@ const footerDom = document.querySelector('#footer');
 bottoneDom.addEventListener('click', function() {
     mainDom.classList.remove('d-none');
     footerDom.classList.remove('d-none');
-}
-);
-for (let i = 1; i <= 100; i++ ) {
-    const quadratino = document.createElement('div');
-    quadratino.classList.add('square');
-    containerDom.append(quadratino);
-    quadratino.append(i);
-    quadratino.addEventListener('click', function() {
-        this.classList.toggle('cliccato');
-        console.log(i);
+    containerDom.innerHTML = '';
+    for (let i = 1; i <= 100; i++ ) {
+    
+        const quadratino = document.createElement('div');
+        quadratino.classList.add('square');
+        quadratino.append(i);
+        containerDom.append(quadratino);
+        
+        quadratino.addEventListener('click', function() {
+    
+            this.classList.toggle('cliccato');
+            console.log(i);
+        }
+        );
+        
     }
-    );
     
 }
+);
 
